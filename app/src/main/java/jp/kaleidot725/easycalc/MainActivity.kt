82711d95621de577c56c92fb6adfb7471e8ec1ab
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-12-24 14:18:58 +0900

    delete app review

--- app/src/main/java/jp/kaleidot725/easycalc/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/easycalc/MainActivity.kt
@@ -7,14 +7,12 @@ import androidx.appcompat.app.AppCompatDelegate
 import androidx.compose.runtime.getValue
 import androidx.core.os.LocaleListCompat
 import androidx.core.view.WindowCompat
-import com.google.android.play.core.review.ReviewManagerFactory
 import jp.kaleidot725.easycalc.compose.app.ComposeApp
 import jp.kaleidot725.easycalc.compose.app.ComposeAppEvent
 import jp.kaleidot725.easycalc.compose.app.ComposeAppViewModel
 import org.koin.androidx.compose.koinViewModel
 import org.orbitmvi.orbit.compose.collectAsState
 import org.orbitmvi.orbit.compose.collectSideEffect
-import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,10 +28,6 @@ class MainActivity : AppCompatActivity() {
                             val list = LocaleListCompat.forLanguageTags(it.language.lang)
                             AppCompatDelegate.setApplicationLocales(list)
                         }
-
-                        ComposeAppEvent.ShowAppInReview -> {
-                            showAppInReview()
-                        }
                     }
                 }
             )
@@ -41,22 +35,6 @@ class MainActivity : AppCompatActivity() {
                 appState = appState,
                 appAction = appViewModel
             )
-        }
-    }
-
-    private fun showAppInReview() {
-        val reviewManager = ReviewManagerFactory.create(this.baseContext)
-        val request = reviewManager.requestReviewFlow()
-        request.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                val reviewInfo = task.result
-                val flow = reviewManager.launchReviewFlow(this, reviewInfo)
-                flow.addOnCompleteListener { result ->
-                    Timber.i("Complete app in review.")
-                }
-            } else {
-                Timber.e("Failed app in review(${task.exception}).")
-            }
         }
     }
 }
